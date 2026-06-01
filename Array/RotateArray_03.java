4aec59fb031eba8a5ed916a34480bf0ffefd173b
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-01 23:06:15 +0530

    Solved Rotate Array

---
+++ Array/RotateArray_03.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class RotateArray_03 {
+    public static void main(String[]args){
+        //Problem name - Rotate Array   
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int result[] = new int[n];
+        for(int i=n-; i>=0; i--)
+        {
+            int val = (i+k)%n;
+            result[val] = nums[i];
+        }
+        for(int i=0; i<result.length; i++)
+        {
+            System.out.println(result[i]);
+        }
+        //complexity - Time: o(n) & space: o(n)
+    }
+}
