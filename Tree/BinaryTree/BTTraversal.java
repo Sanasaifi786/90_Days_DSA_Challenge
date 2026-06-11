42b5c384f74320b5287dff06456f107e6c875b6e
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-11 17:17:05 +0530

    Add binary tree traversal implementation

---
+++ Tree/BinaryTree/BTTraversal.java
@@ -0,0 +1,35 @@
+public class BTTraversal {
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void preorder(Node root){
+        if(root==null)
+        {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args) {
+        //Problem name = Traversal of Binary tree
+        Node root = new BTTraversal().new Node(1);
+        root.left = new BTTraversal().new Node(2);
+        root.right = new BTTraversal().new Node(3);
+        root.left.left = new BTTraversal().new Node(4);
+        root.left.right = new BTTraversal().new Node(5);
+        root.right.left = new BTTraversal().new Node(6);
+        root.right.right = new BTTraversal().new Node(7);
+
+        System.out.println("Preorder Traversal:");
+        preorder(root);
+    }
+}
