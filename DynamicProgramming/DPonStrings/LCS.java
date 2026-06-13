13eba33c43c573cd54a369d0763c022bc7ed6bd1
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-14 01:26:56 +0530

    Refactor LCS implementation to include memoization for improved efficiency

--- DynamicProgramming/DPonStrings/LCS.java
+++ DynamicProgramming/DPonStrings/LCS.java
@@ -16,12 +16,48 @@ public class LCS {
             return Math.max(left,right);
         }
     }
+    public static int lcsDP(String s1,String s2, int n , int m,int dp[][])
+    {
+         if(n==0 || m==0)
+        {
+            return 0;
+        }
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
+        if(s1.charAt(n-1)==s2.charAt(m-1))
+        {
+            dp[n][m] =  1+lcs(s1,s2,n-1,m-1);
+            return dp[n][m];
+        }
+        else{
+            int left = lcs(s1,s2,n-1,m);
+            int right = lcs(s1,s2,n,m-1);
+            dp[n][m] = Math.max(left,right);
+            return dp[n][m];
+        }
+    }
     public static void main(String[] args)
     {
         //problem name - Longest comman Subquence
         String s1 = "abcdgh";
         String s2 = "abedfh";
 
-        System.out.println(lcs(s1,s2,s1.length(),s2.length()));
+        //Recursive
+        // System.out.println(lcs(s1,s2,s1.length(),s2.length()));
+
+        //Dp with memoization
+        int dp[][] = new int[s1.length()+1][s2.length()+1];
+        for(int i=0; i<dp.length; i++)
+        {
+            for(int j = 0; j<dp[i].length; j++)
+            {
+                dp[i][j] = -1;
+            }
+
+        }
+        System.out.println(lcsDP(s1,s2,s1.length(),s2.length(),dp));
+
     }
 }
