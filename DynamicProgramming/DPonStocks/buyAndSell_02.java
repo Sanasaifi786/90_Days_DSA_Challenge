ac45a4f2ea5fe4f49c8af2eaab5a942aa9622c38
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-22 12:38:13 +0530

    Add buyAndSell_02 class for stock buy and sell problem implementation

---
+++ DynamicProgramming/DPonStocks/buyAndSell_02.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class buyAndSell_02 {
+    public static  int  buyAndSell(int prices[],int i,int buy,int [][]dp)
+    {
+        if(i==prices.length)
+        {
+            return 0;
+        }
+        if(dp[i][buy]!=-1)
+        {
+            return dp[i][buy];
+        }
+        if(buy==1)
+        {
+            int canBuy = -prices[i] + buyAndSell(prices,i+1,0,dp);
+            int notBuy = buyAndSell(prices,i+1,1,dp);
+            dp[i][buy] = Math.max(canBuy,notBuy);
+            return dp[i][buy];
+        }
+        else{
+            int sell = prices[i]+buyAndSell(prices,i+1,1,dp);
+            int notSell = buyAndSell(prices,i+1,0,dp);
+            dp[i][buy] = Math.max(sell,notSell);
+            return dp[i][buy];
+        }
+    }
+    public static void main(String[] args)
+    {
+        // Using Memoization
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int prices[] = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            prices[i] = sc.nextInt();
+        }
+        int dp[][] = new int[prices.length][2];
+        for(int[] i: dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        System.out.println(buyAndSell(prices,0,1,dp));
+    }
+}
