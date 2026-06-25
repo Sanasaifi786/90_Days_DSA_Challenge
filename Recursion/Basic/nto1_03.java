883d8ff707368ecfe13c52fb492b5a2e3c486377
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-25 19:03:51 +0530

    Add nto1_03 class for printing numbers in descending order using recursion

---
+++ Recursion/Basic/nto1_03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class nto1_03 {
+    public static void print(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        
+        System.out.println(n);
+        print(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+}
