ebe29a00245ea6d2b760dea0526a65026f8ae286
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-25 19:04:50 +0530

    Add topView class for implementing top view of a binary tree using level order traversal

---
+++ Tree/BinaryTree/Views/topView.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class topView {
+        static class Info{
+            Node node;
+            int hd;
+            public Info(Node node,int hd)
+            {
+                node = this.node;
+                hd = this.hd;
+            }
+        }
+    public static void main(String[] args)
+    {
+        //Approch - using level order traversal
+        Queue<Info> q = new LinkedList<>();
+
+    }
+}
