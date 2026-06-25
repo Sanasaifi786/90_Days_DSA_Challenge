98d4d1bc1b5bbae2ebb18e3bd4f73c8233829cd6
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-25 19:03:28 +0530

    Add fibonacci_05 class for calculating Fibonacci numbers using recursion

---
+++ Recursion/Basic/fibonacci_05.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fibonacci_05 {
+    public static int fibo(int n)
+    {
+        if(n==1 || n==0)
+        {
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
+    }
+}
