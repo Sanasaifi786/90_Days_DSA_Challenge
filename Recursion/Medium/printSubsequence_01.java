fe704690339e4b5902b7f4da9589bf32342a5e97
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-27 21:12:00 +0530

    Refactor printSubsequence_01 class to implement subset_01 method for single subsequence retrieval

--- Recursion/Medium/printSubsequence_01.java
+++ Recursion/Medium/printSubsequence_01.java
@@ -13,9 +13,36 @@ public class printSubsequence_01 {
         list.remove(list.size()-1);
         subset(nums,i+1,list);
     }
+    public static boolean subset_01(int[] nums,int i,ArrayList<Integer> list,int sum)
+    {
+        if(i>=nums.length)
+        {
+            if(sum==0)
+            {
+                System.out.println(list);
+                return true;
+            }
+            return false;
+        }
+        list.add(nums[i]);
+        if(subset_01(nums, i+1, list, sum-nums[i])==true)
+        {
+            return true;   
+        }
+        list.remove(list.size()-1);
+        sum -= nums[i];
+        if(subset_01(nums,i+1,list,sum)==true)
+        {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int nums[] = {3,1,2};
         ArrayList<Integer> list = new ArrayList<>();
         subset(nums,0,list);
+
+        //print only one subsequence
+        subset_01(nums,0,list,2);
     }
 }
