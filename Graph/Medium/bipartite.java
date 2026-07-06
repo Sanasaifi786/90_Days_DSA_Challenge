51c4ecbff43bcd632c68c349f7490da1f8fc4a93
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-07-06 20:55:30 +0530

    Refactor bipartite class to enhance DFS implementation and improve output messages for bipartite graph checking

--- Graph/Medium/bipartite.java
+++ Graph/Medium/bipartite.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class bipartite {
+    //using BFS to check if the graph is bipartite or not
     public static boolean bfs(int [][]graph, int start,int color[])
     {
         Queue<Integer> q = new LinkedList<>();
@@ -27,6 +28,26 @@ public class bipartite {
         }
         return true;
     }
+    //using DFS to check if the graph is bipartite or not 
+    public static boolean dfs(int [][] graph, int start, int color[],int initial)
+    {
+        color[start] = initial;
+        for(int i=0; i<graph[start].length; i++)
+        {
+            if(color[graph[start][i]]==-1)
+            {
+                if(!dfs(graph,graph[start][i],color,1-initial))
+                {
+                    return false;
+                }
+            }
+            else if(color[i]==color[start])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         // Problem name - Bipartite graph
         int [][] graph = {
@@ -38,7 +59,18 @@ public class bipartite {
         int n = graph.length;
         int color[] = new int[n];
         Arrays.fill(color,-1);
-        System.out.println(bfs(graph,0,color));
-        
+        // System.out.println(bfs(graph,0,color));
+        for(int i=0; i<n; i++)
+        {
+            if(color[i] == -1)
+            {
+                if(dfs(graph,i,color,0))
+                {
+                    System.out.println("The graph is bipartite");
+                    return;
+                }
+            }
+        }
+        System.out.println("Not Bipartite");
     }
 }
